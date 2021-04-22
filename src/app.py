e2384dd72f703141600a8ad6d0efbe25014975d1
aaronblanco <aaronba13@hotmail.com>
2021-04-22 08:07:47 +0200

    Actualizado.

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import request
-from .models import Personajes, SableLaser
+from flask import render_template
+from .models import Character, Planets, Starships
 from . import inicio, database
     
 
@@ -9,106 +10,135 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return 'Index'
+    return render_template('index.html')
 
-@app.route('/personajes', methods=['POST'])
-def addpersonaje():
-    personaje = request.get_json()
-    name = personaje['name']
-    edad = personaje['edad']
-    database.add_instance(Personajes, name = name, edad = edad)
+## CHARACTERS
+
+@app.route('/character', methods=['POST'])
+def addCharacter():
+    character = request.get_json()
+    name = character['name']
+    edad = character['edad']
+    gender = character['gender']
+    species = character['species']
+    weapon = character['weapon']
+    database.add_instance(Character, name = name, edad = edad, gender = gender, species = species, weapon = weapon)
    
     return json.dumps("Añadido"), 200
 
 
-@app.route('/personajes', methods=['GET'])
-def getpersonajes():
-    personajes = database.get_all(Personajes)
-    if personajes is None:
-        return json("La bd está vacía.")
+@app.route('/character', methods=['GET'])
+def getCharacter():
+    characters = database.get_all(Character)
+    if characters is None:
+        return json.dumps("BBDD is empty."), 200
 
-    personajes_all = []
-    for personaje in personajes:
-        nuevo_personaje = {
-            "id": personaje.id,
-            "name": personaje.name,
-            "edad": personaje.edad,
-
+    character_all = []
+    for character in characters:
+        new_character = {
+            "id": character.id,
+            "name": character.name,
+            "edad": character.edad,
+            "gender": character.gender,
+            "species": character.species,
+            "weapon": character.weapon,
         }
 
-        personajes_all.append(nuevo_personaje)
-    return json.dumps(personajes_all), 200
+        character_all.append(new_character)
+    return json.dumps(character_all), 200
   
   
-  
- 
 
+@app.route('/character/<string:character_name>', methods=['DELETE'])
+def deleteCharacter(character_name):
+   database.delete_instance(Character, name=character_name)
 
+## PLANETS
 
-@app.route('/personajes/<string:personaje_name>', methods=['DELETE'])
-def deletepersonaje(personaje_name):
-   database.delete_instance(Personajes, name=personaje_name)
-
-
-@app.route('/sablesLaser', methods=['POST'])
-def addLaser():
-    SableLaser = request.get_json()
-    color = SableLaser['color']
-    database.add_instance(SableLaser, color = color)
+@app.route('/planet', methods=['POST'])
+def addPlanet():
+    planet = request.get_json()
+    name = planet['name']
+    population =  planet['population']
+    diameter = planet['diameter']
+    species =  planet['species']
+    database.add_instance(Planets, name = name, population = population, diameter = diameter, species = species)
    
     return json.dumps("Añadido"), 200
 
 
-@app.route('/sablesLaser', methods=['GET'])
-def getsables():
-    SablesLaser = database.get_all(SableLaser)
-    if SablesLaser is None:
-        return json("La bd está vacía.")
+@app.route('/planet', methods=['GET'])
+def getPlanet():
+    planets = database.get_all(Planets)
+    if planets is None:
+        return json.dumps("BBDD is empty."), 200
 
-    sableLaser_all = []
-    for sable in SablesLaser:
-        nuevo_sable = {
-            "id": sable.id,
-            "color": sable.name,
-           
-
+    planet_all = []
+    for planet in planets:
+        new_planet = {
+            "id": planet.id,
+            "name": planet.name,
+            "population": planet.population,
+            "diameter": planet.diameter,
+            "species": planet.species,
         }
 
-        sableLaser_all.append(nuevo_sable)
-    return json.dumps(sableLaser_all), 200
-  
-  
+        planet_all.append(new_planet)
+    return json.dumps(planet_all), 200
   
  
+@app.route('/planet/<string:planet_name>', methods=['DELETE'])
+def deletesables(planet_name):
+   database.delete_instance(Planets, name=planet_name)
+
+## STARSHIPS
+
+@app.route('/starship', methods=['POST'])
+def addStarship():
+    starship = request.get_json()
+    name = starship['name']
+    model =  starship['model']
+    length = starship['length']
+    starship_class =  starship['starship_class']
+    database.add_instance(Starships, name = name, model = model, length = length, starship_class = starship_class)
+   
+    return json.dumps("Añadido"), 200
 
 
+@app.route('/starship', methods=['GET'])
+def getStarship():
+    starships = database.get_all(Starships)
+    if starships is None:
+        return json.dumps("BBDD is empty."), 200
 
-@app.route('/sablesLaser/<string:sablesLaser_color>', methods=['DELETE'])
-def deletesables(sablesLaser_color):
-   database.delete_instance(Personajes, color=sablesLaser_color)
+    starship_all = []
+    for starship in starships:
+        new_planet = {
+            "id": starship.id,
+            "name": starship.name,
+            "model": starship.model,
+            "length": starship.length,
+            "starship_class": starship.starship_class,
+        }
+
+        starship_all.append(new_planet)
+    return json.dumps(starship_all), 200
+  
+
+@app.route('/starship/<string:starship_name>', methods=['DELETE'])
+def deleteStarship(starship_name):
+   database.delete_instance(Starships, name=starship_name)
+
+#Ejemplo prueba.
+"""
+
+{
+	"name" : "Obi-Wan Kenobi",
+    "edad" : 35,
+    "gender" : "Hombre",
+    "species" : "Humano",
+    "weapon" : "Sable Laser"
+}
 
 
 """
-  
-@app.route('/personajes/<string:personaje_name>', methods=['PUT'])
-def editpersonaje(personaje_name):
-    personajesFound = [personaje for personaje in personajes if personaje['name'] == personaje_name]
-    if (len(personajesFound) > 0):
-        personajesFound[0]['name'] = request.json['name']
-        personajesFound[0]['edad'] = request.json['edad']
-        return jsonify({
-            'message': 'personaje Updated',
-            'personaje': personajesFound[0]
-        })
-    return jsonify({'message': 'personaje Not found'})
-
-@app.route('/personajes/<string:personaje_name>')
-def getpersonaje(personaje_name):
-    personajesFound = [
-        personaje for personaje in personajes if personaje['name'] == personaje_name.lower()]
-    if (len(personajesFound) > 0):
-        return jsonify({'personaje': personajesFound[0]})
-    return jsonify({'message': 'personaje Not found'})  
-        
-
-""" 
--- src/models.py
+++ src/models.py
@@ -3,15 +3,27 @@ import flask_sqlalchemy
 db = flask_sqlalchemy.SQLAlchemy()
 
 
-class Personajes(db.Model):
-    __tablename__ = 'personajes'
+class Character(db.Model):
+    __tablename__ = 'character'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
     edad = db.Column(db.Integer)
+    gender = db.Column(db.String(100))
+    species = db.Column(db.String(100))
+    weapon = db.Column(db.String(100))
 
-class SableLaser(db.Model):
-    __tablename__ = 'Sables'
+class Planets(db.Model):
+    __tablename__ = 'planets'
     id = db.Column(db.Integer, primary_key=True)
-    color = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    population = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    species = db.Column(db.String(100))
 
-
+class Starships(db.Model):
+    __tablename__ = 'starships'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    model = db.Column(db.String(100))
+    length = db.Column(db.Integer)
+    starship_class = db.Column(db.String(100))
